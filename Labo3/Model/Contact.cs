6a29fb42e9d88a767c28e1173eb20cd5c310886c
DamienJacques <damien.jacques900@gmail.com>
2016-09-26 12:22:39 +0200

    Labo3

---
+++ Labo3/Model/Contact.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Contact
+    {
+        /*
+        private String firstName;
+        private String lastName;
+        private String mail;
+        */
+
+        public String FirstName
+        {
+            get;
+            set;
+        }
+
+        public String LastName
+        {
+            get;
+            set;
+        }
+
+        public String Mail
+        {
+            get;
+            set;
+        }
+
+        public Contact(String firstName, String lastName, String mail)
+        {
+            FirstName = firstName;
+            LastName = LastName;
+            Mail = mail;
+        }
+    }
+}
