1850c88de0d468be6b7b68da502619ff26a50057
DamienJacques <damien.jacques900@gmail.com>
2016-10-10 11:39:53 +0200

    Labo 5

---
+++ Labo5/Labo5/Model/Forecast.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo5.Model
+{
+    class Forecast
+    {
+        public String CityName
+        {
+            get; set;
+        }
+
+        public IEnumerable<WeatherForecast> WeatherForecasts
+        {
+            get; set;
+        }
+    }
+}
